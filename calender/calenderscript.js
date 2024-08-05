5c6a42bab59999c47763189c33d0158db032e430
sophiastudent123 <125307995+sophiastudent123@users.noreply.github.com>
2024-08-05 09:37:06 +0800

    added js

--- calender/calenderscript.js
+++ calender/calenderscript.js
@@ -1,54 +1,79 @@
-const monthYearElement = document.getElementById('monthYear')
-const datesElement = document.getElementById('dates')
-const prevBtn = document.getElementById('prevBtn')
-const nextBtn = document.getElementById('nextBtn')
+const isLeapYear  = (year) => {
+    return (
+        (year % 4 === 0 && year % 100 !==0 && year % 400 !== 0)||
+        (year % 100 === 0 && year % 400 === 0) 
+    );
+};
 
-
-let currentDate = new Date()
-
-const updateCalender = () => {
-    const currentYear = currentDate.getFullYear()
-    const currentMonth = currentDate.getMonth()
-
-    const firstDay = new Date(currentYear, currentMonth, 0)
-    const lastDay = new Date(currentYear. currentMonth+1, 0)
-    const totalDays = lastDay.getDate()
-    const firstDayIndex = firstDay.getDay()
-    const lastDayIndex = lastDay.getDay()
-
-    const monthYearString = currentDate.toLocaleString
-    ('default', {month: 'long', year: 'numeric'})
-    monthYearElement.textContent = monthYearString
-
-    let datesHTML = "";
-
-    for (let i = firstDayIndex; i>0; i--){
-
-        const prevDate = new Date(currentYear, currentMonth, 0 - i + 1)
-        datesHTML += `<div class = "date inactive">${prevDate.getDate()}</div>`
-    }
-    for (let i = 1; i<=totalDays; i++){
-        const date = new Date(currentYear, currentMonth, i)
-        const activeClass = date.toDateString() === new Date().toDateString() ? 'active':''
-        datesHTML += `<div class="date ${activeClass}"> ${i}</div>`
-
-    }
-
-    for (let i = 1; i<= (7 - lastDayIndex); i++) {
-        const nextDate = new Date(currentYear, currentMonth + 1, i)
-        datesHTML += `<div class= "date inactive"> ${nextDate.getDate()}</div>`
-
-    }
-    datesElement.innerHTML = datesHTML
+const getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28;
 }
 
-prevBtn.addEventListener('click', ()=>{
-    currentDate.setMonth(currentDate.getMonth() - 1)
-    updateCalender()
-})
+let calender = document.querySelector('.calender')
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
 
-nextBtn.addEventListener('click', ()=>{
-    currentDate.setMonth(currentDate.getMonth() + 1)
-    updateCalender()
-})
-updateCalender()
+let month_picker = document.querySelector('#month-picker')
+let dayTextFormate = document.querySelector('.day-text-formate')
+let timeFormate = document.querySelector('time-formate')
+let dateFormate = document.querySelector('.date-formate')
+
+month_picker.onclick = () => {
+    month_list.classList.remove('hideonce')
+    month_list.classList.remove('hide')
+    month_list.classList.add('show')
+    dayTextFormate.classList.remove('showtime')
+    dayTextFormate.classList.add('hidetime')
+    timeFormate.classList.remove('showtime')
+    timeFormate.classList.add('hideTime')
+    dateFormate.classList.remove('showtime')
+    dateFormate.classList.add('hideTime')
+}
+
+const generateCalender = (month, year) => {
+    let calender_days = document.querySelector('.calender-days')
+    calender_days.innerHTML = ""
+    let calender_header_year = document.querySelector('#year')
+    let days_of_month = [
+        31, 
+        getFebDays(year),
+        31,
+        30,
+        31,
+        30,
+        31,
+        31,
+        30,
+        31,
+        30,
+        31
+    ];
+    let currentDate = new Date()
+    month_picker.innerHTML = month_names[month]
+    calender_header_year.innerHTML = year
+    let first_day = new Date (year, month)
+
+    for (let i =0; i<=days_of_month[month]+first_day.getDay()-1; i++){
+        let day = document.createElement('div')
+
+        if (i >= first_day.getDay()){
+            day.innerHTML = i - first_day.getDay() + 1
+            if (i - first_day.getDay() + 1){//1930
+
+            }
+        }
+
+    }
+}
